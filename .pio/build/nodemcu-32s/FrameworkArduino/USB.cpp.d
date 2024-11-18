7ba9c69e2934eb61ea5a282ae4917e7e2692f018
sailingfree <peter@naiadhome.com>
2024-11-18 14:19:49 +0000

    Remove .pio from git

--- .pio/build/nodemcu-32s/FrameworkArduino/USB.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/nodemcu-32s/FrameworkArduino/USB.cpp.o: \
- /Users/petermartin_2/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.cpp \
- /Users/petermartin_2/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- /Users/petermartin_2/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/nodemcu-32s/FrameworkArduino/firmware_msc_fat.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/nodemcu-32s/FrameworkArduino/firmware_msc_fat.c.o: \
- /Users/petermartin_2/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
- /Users/petermartin_2/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
