42f484a5f4b08ebfc4e6abecebe60723440e41f1
sailingfree <peter@naiadhmoe.com>
2026-01-15 21:30:13 +0000

    Add version info

---
+++ include/Version.h
@@ -0,0 +1,9 @@
+
+    // AUTO GENERATED FILE, DO NOT EDIT
+    #ifndef VERSION
+        #define VERSION "0.1.4"
+    #endif
+    #ifndef BUILD_TIMESTAMP
+        #define BUILD_TIMESTAMP "2026-01-15 21:28:40.178942"
+    #endif
+    
